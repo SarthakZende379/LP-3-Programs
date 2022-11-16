56a8fe564b81083a94012c2b9bd5a35ef325c3f4
SarthakZende379 <74917835+SarthakZende379@users.noreply.github.com>
2022-11-16 18:02:41 +0530

    Add files via upload

---
+++ DAA_1_fibonaaci.py
@@ -0,0 +1,24 @@
+#Non-recursive Fibonaaci
+
+#a=int(input("Enter the first number of the series "))
+#b=int(input("Enter the second number of the series "))
+#n=int(input("Enter the number of terms needed "))
+#print(a,b,end=" ")
+#while(n-2):
+#    c=a+b
+#    a=b
+#    b=c
+#    print(c,end=" ")
+#    n=n-1
+
+#Recursive Fibonaaci
+
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter number of terms:"))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i))
